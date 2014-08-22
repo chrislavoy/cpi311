e3f625d82cfc4ec6cd877b87386048556eb46451
rrkamath@asu.edu <rrkamath@asu.edu@ba4156f4-caf2-72f1-87dd-37b8f1b46382>
2014-08-22 16:44:33 +0000

    git-svn-id: http://cpi311-fall2014.googlecode.com/svn/trunk@2 ba4156f4-caf2-72f1-87dd-37b8f1b46382

---
+++ Class Work/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Class Work")]
+[assembly: AssemblyProduct("Class Work")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("ASU")]
+[assembly: AssemblyCopyright("Copyright © ASU 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1741125e-99d6-4086-8790-727e1a4ae62d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
