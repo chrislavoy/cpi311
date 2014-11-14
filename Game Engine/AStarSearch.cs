1f3bd59f7d578904f0ff99835b8cbcccf82a4a0d
rrkamath@asu.edu <rrkamath@asu.edu@ba4156f4-caf2-72f1-87dd-37b8f1b46382>
2014-11-14 02:58:11 +0000

    git-svn-id: http://cpi311-fall2014.googlecode.com/svn/trunk@34 ba4156f4-caf2-72f1-87dd-37b8f1b46382

---
+++ Game Engine/AStarSearch.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CPI311.GameEngine
+{
+    public class AStarSearch
+    {
+        public int Cols { get; set; }
+        public int Rows { get; set; }
+        public AStarNode[,] Nodes { get; set; }
+        public AStarNode Start { get; set; }
+        public AStarNode End { get; set; }
+
+        private SortedDictionary<float, List<AStarNode>> openList;
+
+        public AStarSearch(int rows, int cols)
+        {
+            openList = new SortedDictionary<float, List<AStarNode>>();
+            Rows = rows;
+            Cols = cols;
+            Nodes = new AStarNode[Rows, Cols];
+            for (int r = 0; r < Rows; r++)
+                for (int c = 0; c < Cols; c++)
+                    Nodes[r, c] = new AStarNode(c, r, new Vector3(c, 0, r));
+        }
+
+        public void Search()
+        {
+            #region Initialize grid
+            openList.Clear();
+            foreach(AStarNode node in Nodes)
+            {
+                node.Closed = false;
+                node.Cost = Single.MaxValue;
+                node.Parent = null;
+                node.Heuristic = Vector3.Distance(node.Position, End.Position);
+            }
+            #endregion
+
+            AddToOpenList(Start);
+            while(openList.Count > 0) // openlist is not empty
+            {
+                AStarNode node = GetBestNode();
+                if (node == End)
+                    break;
+                if(node.Row < Rows -1)
+                    AddToOpenList(Nodes[node.Row + 1, node.Col], node);
+                if(node.Row > 0)
+                    AddToOpenList(Nodes[node.Row - 1, node.Col], node);
+                if(node.Col < Cols - 1)
+                    AddToOpenList(Nodes[node.Row, node.Col + 1], node);
+                if(node.Col > 0)
+                    AddToOpenList(Nodes[node.Row, node.Col - 1], node);
+            }
+        }
+
+        private void AddToOpenList(AStarNode node, AStarNode parent = null)
+        {
+            if (!node.Passable || node.Closed) return;
+            if (parent == null) node.Cost = 0;
+            else
+            {
+                float cost = parent.Cost + 1;
+                if (node.Cost > cost)
+                {
+                    RemoveFromOpenList(node);
+                    node.Cost = cost;
+                    node.Parent = parent;
+                }
+                else
+                    return;
+            }
+            if (!openList.ContainsKey(node.Cost+node.Heuristic))
+                openList[node.Cost+node.Heuristic] = new List<AStarNode>();
+            openList[node.Cost+node.Heuristic].Add(node);
+        }
+
+        private AStarNode GetBestNode()
+        {
+            AStarNode node = openList.ElementAt(0).Value[0];
+            openList.ElementAt(0).Value.Remove(node);
+            if (openList.ElementAt(0).Value.Count == 0)
+                openList.Remove(node.Cost+node.Heuristic);
+            node.Closed = true;
+            return node;
+        }
+
+        private void RemoveFromOpenList(AStarNode node)
+        {
+            if (openList.ContainsKey(node.Cost+node.Heuristic))
+            {
+                openList[node.Cost+node.Heuristic].Remove(node);
+                if (openList[node.Cost+node.Heuristic].Count == 0)
+                    openList.Remove(node.Cost+node.Heuristic);
+            }
+        }
+    }
+}
--- Lab 09/Lab09.cs
+++ Lab 09/Lab09.cs
@@ -16,6 +16,16 @@ namespace CPI311.Labs
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        AStarSearch search;
+        List<Vector3> path;
+
+        Random random = new Random();
+
+        Camera camera;
+
+        Model ground;
+        Model cube;
+        Model sphere;
 
         public Lab09()
             : base()
@@ -28,12 +38,37 @@ namespace CPI311.Labs
         {
             Time.Initialize();
             InputManager.Initialize();
+            ScreenManager.Initialize(graphics);
+
+            search = new AStarSearch(100, 100);
+            
+            foreach(AStarNode node in search.Nodes)
+                if(random.NextDouble() < 0.2)
+                search.Nodes[random.Next(100), random.Next(100)].Passable = false;
+            search.Start = search.Nodes[0, 0]; search.Start.Passable = true;
+            search.End = search.Nodes[99, 99]; search.End.Passable = true;
+            search.Search();
+            path = new List<Vector3>();
+            AStarNode current = search.End;
+            while(current != null)
+            {
+                path.Insert(0, current.Position);
+                current = current.Parent;
+            }
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            cube = Content.Load<Model>("Models/Box");
+            //ground = Content.Load<Model>("Models/Box");
+            sphere = Content.Load<Model>("Models/Sphere");
+
+            camera = new Camera();
+            camera.Transform = new Transform();
+            camera.Transform.LocalPosition = Vector3.One * 50;
+            camera.Transform.Rotate(Vector3.Right, -MathHelper.PiOver2);
         }
 
         protected override void Update(GameTime gameTime)
@@ -42,15 +77,41 @@ namespace CPI311.Labs
             InputManager.Update();
             if (InputManager.IsKeyDown(Keys.Escape))
                 Exit();
+            if(InputManager.IsKeyPressed(Keys.Space))
+            {
+                while(!(search.Start = search.Nodes[random.Next(search.Cols), random.Next(search.Rows)]).Passable);
+                while(!(search.End = search.Nodes[random.Next(search.Cols), random.Next(search.Rows)]).Passable);
+                search.Search();
+                path.Clear();
+                AStarNode current = search.End;
+                while (current != null)
+                {
+                    path.Insert(0, current.Position);
+                    current = current.Parent;
+                }
+            }
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-            spriteBatch.Begin();
 
-            spriteBatch.End();
+            Matrix view = camera.View;
+            Matrix projection = camera.Projection;
+
+            (cube.Meshes[0].Effects[0] as BasicEffect).DiffuseColor = Color.DarkBlue.ToVector3();
+            cube.Draw(Matrix.CreateScale(100, 0.1f, 100) * Matrix.CreateTranslation(50, -5, 50), view, projection);
+            (cube.Meshes[0].Effects[0] as BasicEffect).DiffuseColor = Color.DarkRed.ToVector3();
+            foreach (AStarNode node in search.Nodes)
+                if (!node.Passable)
+                    cube.Draw(Matrix.CreateScale(0.5f,0.05f,0.5f)*Matrix.CreateTranslation(node.Position), view, projection);
+            (sphere.Meshes[0].Effects[0] as BasicEffect).DiffuseColor = Color.WhiteSmoke.ToVector3();
+            foreach(Vector3 position in path)
+                sphere.Draw(Matrix.CreateScale(0.1f,0.1f,0.1f)*Matrix.CreateTranslation(position), view, projection);
+            //spriteBatch.Begin();
+
+            //spriteBatch.End();
             base.Draw(gameTime);
         }
     }
